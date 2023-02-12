362c8372abfdb382ea05a315ad62a443f0c5b883
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-02-12 15:54:42 +0300

    Time: 894 ms (69.90%), Space: 28.1 MB (77.17%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        x=[0]*(n+1) # x = [0,0,0,0,0,0]
+        for booking in bookings:#[1,2,10]
+            l,r,seats=booking[0]-1,booking[1],booking[2]
+            x[l]+=seats
+            x[r]-=seats #[10,45,-10,-20,0,-25] 
+        ans=[]
+        prefixsum=0
+        for i in x:
+            prefixsum+=i
+            ans.append(prefixsum)
+        ans.pop()
+        return ans
+    
+            
+        

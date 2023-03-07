5f5b31f018a295f0dd8d50829fe9faa56f3c390a
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-03-07 19:50:29 +0300

    Time: 50 ms (45.02%), Space: 16.4 MB (72.01%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,21 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def inorderTraversal(root: Optional[TreeNode]) -> List[int]:
-            left = []
-            right = []
-            if not root :
-                return []
-            if root.left:
-                left = inorderTraversal(root.left)
-            if root.right:
-                right = inorderTraversal(root.right)
-            return left + [root.val] + right
-        arr = inorderTraversal(root)
-        if (all(i < j for i, j in zip(arr, arr[1:]))):
-            if arr == sorted(arr) :
-
-                return True
-            else:
-                return False
-        return False
+        def validity(Node, minimum = -inf, maximum = inf):
+            if not Node:
+                return True 
+            if Node.val <= minimum or Node.val >= maximum:
+                return False 
+            left_validity= validity(Node.left, minimum, Node.val)
+            right_validity = validity(Node.right, Node.val, maximum)
+            return left_validity and right_validity 
+        return validity(root)
+        

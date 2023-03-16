9c1f766f3aaa0a4b3ef7eb65d327c2a1e2e18101
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-03-16 11:12:17 +0300

    Time: 8421 ms (5.13%), Space: 13.8 MB (98.19%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,28 +1,10 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        def player(start,end,turn):
-            if start == end:
-                if turn:
-                    return [nums[start],0]
-                else:
-                    return [0,nums[start]]
-            if turn:
-                ans1 = player(start+1,end,False)
-                ans1[0]+=nums[start] 
-                ans2 = player(start, end-1, False)
-                ans2[0] += nums[end] 
-                if ans1[0]>ans2[0]: return ans1
-                return ans2
-                
-            else:
-                ans1 = player(start+1,end,True)
-                ans1[1]+=nums[start] 
-                ans2 = player(start, end-1, True)
-                ans2[1]+=nums[end] 
-                if ans1[1]>ans2[1]: return ans1
-                return ans2
-        answer = player(0,len(nums)-1, True)
-        if answer[0] >= answer[1]: return True
-        return False 
-                
-        
+        if not nums:
+            return None 
+        def largescore(left, right):
+            if left > right:
+                return 0
+            return max ((nums[left] - largescore(left + 1, right)), nums[right] - largescore(left, right -1))
+        return largescore(0, len(nums)-1)>=0
+            

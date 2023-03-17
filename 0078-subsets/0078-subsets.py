b8fdf5e46de67a1557261cfa012289ed37f34d34
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-03-17 09:58:37 +0300

    Time: 30 ms (91.39%), Space: 14.1 MB (77.85%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,13 +3,12 @@ class Solution:
         ans = []
         def backtrack(indx, track):
             if indx >= n:
-                ans.append(track)
+                ans.append(track.copy())
                 return 
-            backtrack(indx+1,track)
-            backtrack(indx+1, track +[nums[indx]])
-        
-            
-        n = len(nums)
+            track.append(nums[indx])
+            backtrack(indx+1, track)
+            track.pop()
+            backtrack(indx+1, track)
+        n =len(nums)
         backtrack(0, [])
-        return ans
-    
+        return ans 

5c2467fc0babfd99b8d8354c4836bde7278771bb
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-03-27 17:36:07 +0300

    Time: 102 ms (5.23%), Space: 13.8 MB (71.31%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        ans = []
+        res = []
+        
+        def backtrack(num):
+            
+            if not num:
+                if len(ans) == 4 :
+                    res.append(".".join(list(map(str,ans))))
+                if len(ans) > 4:
+                    return
+             
+            for i in range(1, len(num) +1 ):
+                if len(str(int(num[:i]))) == i and (int(num[:i]) < 256):
+                    ans.append(int(num[:i]))
+                    backtrack(num[i:])
+                    ans.pop()
+        backtrack(s)
+        return res
+        
+        
+            

ac0e4d952eafaa93eb5b97c5124bd1942b850db1
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-04-05 19:07:34 +0300

    Time: 160 ms (29.11%), Space: 15.7 MB (81.33%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,10 +1,24 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        dic = defaultdict(int)
+        arr = [0] * 32 
+        _num = 0
+        for i in nums:
+            if i < 0:
+                _num += 1
+            for j in range(32):
+                if abs(i) & (1 << j ) != 0:
+                    arr[j]+= 1
         
-        for num in nums:
-            dic[num] += 1
-        for i in dic:
-            if dic[i] == 1:
-                return i
+        element = 0
+        for k in range(32):
+            if arr[k] % 3 != 0:
+                element += pow(2, k)
+        if _num % 3 != 0:
+            element = -element
+        return element
+                
+            
+        
+                
+
             

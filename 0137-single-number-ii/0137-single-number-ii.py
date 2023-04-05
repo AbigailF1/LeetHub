9a9ab47e20aa4eddac23c9e4904c9ded7a465c37
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-04-05 18:02:33 +0300

    Time: 54 ms (92.07%), Space: 16 MB (42.40%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic = defaultdict(int)
+        
+        for num in nums:
+            dic[num] += 1
+        for i in dic:
+            if dic[i] == 1:
+                return i
+            

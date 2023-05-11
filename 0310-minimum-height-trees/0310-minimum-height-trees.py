bed4cee6bb22fcfccd6d4185b6a7b88689e4055f
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-05-11 21:16:32 +0300

    Time: 606 ms (17.00%), Space: 29.6 MB (9.21%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,27 +1,38 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-        if n <= 2:
-            return list(range(n))
         dic = defaultdict(list)
         for i,j in edges:
             dic[i].append(j)
             dic[j].append(i)
             
-         
+        max_height = [0]*n
+        edge_count = [0]*n
+        
+        for i in range(len(dic)):
+            edge_count[i] = len(dic[i])
+            
+        visited = set()  
         queue = deque()
         for i in range(len(dic)):
-            if len(dic[i]) == 1:
+            if edge_count[i] == 1:
                 queue.append(i)
-        nodes = n
-        while nodes > 2:
-            for i in range(len(queue)):
-                x = queue.popleft()
-                nodes -= 1
-                
-                parent = dic[x].pop()
-                dic[parent].remove(x)
-                
-                if len(dic[parent])== 1:
-                    queue.append(parent)
-        return queue
-            
+                visited.add(i)
+        left_edges = n-1
+        while left_edges > 1 :
+            x = queue.popleft()
+            left_edges-=1
+            for i in dic[x]:
+                edge_count[i]-=1
+                if i not in visited:
+                    max_height[i] = max(max_height[i] ,max_height[x]+1)
+                if edge_count[i] == 1:
+                    queue.append(i)
+                    visited.add(i)
+        ans = []
+        maxx = max(max_height)
+        for i in range(len(max_height)):
+            if maxx == max_height[i]:
+                ans.append(i)
+                    
+        print(max_height)
+        return ans

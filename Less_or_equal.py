d629f67f9d15bc217e4ed6b7dc3e4767c2408e76
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-05-14 17:25:19 +0300

    Create Less_or_equal.py

---
+++ Less_or_equal.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+if k == 0:
+    if k == 0:
+        if arr[0] <= 1:
+            print(-1)
+        else:
+            print(arr[0] - 1)
+elif k == n:
+    if k == n:
+        print(arr[k - 1])
+else:
+    if arr[k] <= arr[k-1]:
+        print(-1)
+    else:
+        print(arr[k-1])

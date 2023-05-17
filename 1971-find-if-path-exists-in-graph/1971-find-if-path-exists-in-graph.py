32807d64a5f1c458de662c1f4ff71eb7a05e70df
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-05-17 18:47:28 +0300

    Time: 1893 ms (72.77%), Space: 105.9 MB (83.40%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,28 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        path = defaultdict(list)
-        for edge in edges:
-            path[edge[0]].append(edge[1])
-            path[edge[1]].append(edge[0])
-            
-        visited = set() 
-        def check_path(source, visited):
-            if source in visited:
-                return
-            visited.add(source)
-    
-            if source == destination:
-                return True
-            for num in path[source]:
-                find = check_path(num, visited)
-
-                if find :
-                    return True
-            return False
-        return check_path(source, visited)
+        rep = {i:i for i in range(n)}
+        rank = [1]* n
+        
+        def find(x):
+            if rep[x] != x:
+                rep[x] = find(rep[x])
+            return rep[x]
+        
+        def union(x, y):
+            x_rep = find(x)
+            y_rep = find(y)
+        
+            if x_rep != y_rep:
+                if rank[x_rep] >= rank[y_rep]:
+                    rep[y_rep] = rep[x_rep]
+                    rank[x_rep] += rank[y_rep]
+                else:
+                    rep[x_rep] = rep[y_rep]
+                    rank[y_rep] += rank[x_rep]                               
+                
+        for i,j in edges:
+            union(i,j)        
+        return find(source) == find(destination)
         
                     
                     

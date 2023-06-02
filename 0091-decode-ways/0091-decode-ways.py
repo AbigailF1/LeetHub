d55ba54ae288ae34ff609f5c99594ce36166c0e9
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-06-02 19:11:38 +0300

    Time: 53 ms (19.99%), Space: 16.4 MB (18.13%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         valid = set([str(x) for x in range(1,27)])
-        memo = defaultdict(int)
-        
-        def dp(i):
-            if i == len(s):
-                return 1
-            if i not in memo:
-                take_1, take_2 = 0,0    
-                if s[i] != '0': 
-                    take_1 = dp(i+1)
-                if i + 1 < len(s) and s[i:i+2] in valid:
-                    take_2 = dp(i+2)
-                memo[i] = take_1 + take_2 
-            return memo[i]
-            
-        
-        return dp(0)
+        memo = [0] * (len(s) + 1)
+        memo[-1] = 1
+        for i in range(len(s) -1, -1,-1):
+            if s[i] != '0':
+                memo[i] += memo[i+1]
+            if i + 1 < len(s) and s[i:i+2] in valid:
+                memo[i] += memo[i+2]     
+        return memo[0]
         
             
                 

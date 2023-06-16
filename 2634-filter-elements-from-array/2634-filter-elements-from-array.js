68a3fffe016d3dd336f119376d32e357d9410062
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-06-16 21:01:59 +0300

    Time: 63 ms (29.37%), Space: 42.2 MB (23.39%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const new_arr = new Array(arr.length)
+    for(let i = 0; i < arr.length; i++){
+        new_arr[i] = fn(arr[i], i)
+    }
+    const ans = []
+    for(let i = 0; i < arr.length; i++){
+        if (new_arr[i] != false){
+            ans.push(arr[i])
+        }
+    }
+    console.log(new_arr)
+    return ans
+    
+};

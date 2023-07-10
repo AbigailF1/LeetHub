cf07f12389569569acebbce60ca7bc7e823ba86b
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-07-10 18:22:29 +0300

    Time: 74 ms (54.89%), Space: 16.5 MB (65.40%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,16 +1,11 @@
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = {}
+        memo = {0:cost[0], 1:cost[1]}
         cost.append(0)
-        def fib(i):
-            if i == 0 or i== 1:
-                memo[i] = cost[i]
-            if i not in memo:
-                if fib(i-1) > fib(i -2):
-                    memo[i] = fib(i-2) + cost[i]
-                else:
-                    memo[i] = fib(i-1) + cost[i]
-            return memo[i]
-        return fib(len(cost) -1)
+        for i in range(2,len(cost)):
+            memo[i] = min(memo[i-1], memo[i-2]) + cost[i]
+        return memo[i]
+                        
+    
         

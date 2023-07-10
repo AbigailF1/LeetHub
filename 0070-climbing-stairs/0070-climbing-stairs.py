aadfe323c810319f16f9373360ed78cd716484da
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-07-10 17:50:47 +0300

    Time: 46 ms (57.09%), Space: 16.3 MB (68.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,10 @@
-memo ={}
+memo ={1:1, 2:2}
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n < 3:
-            return n
-        if n not in memo:
-            memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
-        return memo[n]
+        for i in range(3,n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]    
+       
+        
         
         

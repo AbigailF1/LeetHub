c6fe55e454f370fe0b618dc6eff1fdc99c79b1a6
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-09-29 18:25:29 +0300

    Time: 2129 ms (16.90%), Space: 530.5 MB (5.00%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s) -1
+        s2 = s[::-1]
+        print(s2)
+        m = len(s2) - 1 
+        memo = {}
+        # dp function 
+        def longest(i,j):
+            if i < 0 or j < 0:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            if s[i] == s2[j]:
+                memo[(i,j)] = 1 + longest(i-1,j-1)
+            else:
+                memo[(i,j)] = max(longest(i,j-1), longest(i -1 ,j))
+            return memo[(i,j)]
+        return longest(n,m)
+        

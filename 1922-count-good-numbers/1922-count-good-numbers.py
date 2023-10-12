e1d8ca21b2589da4d64fb0569996e4bf92c5d46c
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-10-12 12:02:18 +0300

    Time: 52 ms (7.28%), Space: 16.3 MB (57.04%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,10 +1,22 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
+        
+        def multiply(a, b, m):
+            return ((a % m) * (b % m)) % m
+
+        def binary_exponentiation( base, exponent, mod):
+            result = 1
+            while exponent > 0:
+                if exponent & 1:
+                    result = multiply(base, result, mod)
+                base = multiply(base, base, mod) 
+                exponent >>= 1
+            return result
+        
         if n % 2 == 0:
-            return (pow(5,(n//2),(10**9 + 7 )) * pow(4, (n//2), (10**9 + 7)))% (10**9 + 7)
+            return binary_exponentiation( 5, n//2, 10**9 + 7 ) * binary_exponentiation( 4, n//2, 10**9 + 7 )% (10**9 + 7)
         else:
             if n == 1:
-                return(pow(5,((n//2 )+ 1),(10**9 + 7) ))
+                return binary_exponentiation( 5, (n//2 + 1), 10**9 + 7 ) 
             else:
-                return ((pow(5,((n//2 )+ 1),(10**9 + 7 ))) * (pow(4, (n//2), (10**9 + 7)))) % (10**9 + 7)
-        
+                return binary_exponentiation( 5, (n//2 + 1), 10**9 + 7 )  * binary_exponentiation( 4, n//2, 10**9 + 7 )% (10**9 + 7)

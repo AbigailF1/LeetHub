a94b92f9708ec7deaba3b2bfd2935d1bca0c0652
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-10-12 11:21:42 +0300

    Time: 48 ms (6.36%), Space: 16.2 MB (57.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,13 +1,21 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1 
-        elif  n == 1:
-            return x 
+        result = 1
+        def calculate(x,n):
+            nonlocal result
+            while n > 0:
+                if n & 1:
+                    result *= x
+
+                x *= x
+                n>>=1
+                
+            
         if n > 0:
-            number = self.myPow(x,n//2)
-            if n % 2 == 0:
-                return number * number
-            return x* number * number   
-        return 1/(self.myPow(x,abs(n)))
-        
+            calculate(x,n)
+            return result
+        elif n <  0:
+            calculate (x, (-n))
+            return 1/result
+        return result
+            

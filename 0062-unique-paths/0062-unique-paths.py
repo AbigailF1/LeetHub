b0a8738f7da78a6081c3d3a7c6b435e513260444
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-10-12 11:05:26 +0300

    Time: 32 ms (91.16%), Space: 16.2 MB (67.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,5 @@
+from math import factorial 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int: 
-        memo = defaultdict(int)
-        def dp(x,y):
-            if x + y <= 1: return 1
-            state = (x,y)
-            if state not in memo:
-                if x >= 1:memo[state] += dp(x-1,y)
-                if y >= 1: memo[state] += dp(x,y-1)
-            return memo[state]
-        return dp(n-1,m-1)
+    def uniquePaths(self, m: int, n: int) -> int:
+        return factorial((m-1) + ( n-1))//(factorial((m-1)) * factorial((n-1)))
+        

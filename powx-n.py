d60a6c2cc547353182c8cf7afd2b5f462ed75a25
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-10-18 22:22:23 +0300

    added powx-n.python

---
+++ powx-n.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        result = 1
+        def calculate(x,n):
+            nonlocal result
+            while n > 0:
+                if n & 1:
+                    result *= x
+
+                x *= x
+                n>>=1
+                
+            
+        if n > 0:
+            calculate(x,n)
+            return result
+        elif n <  0:
+            calculate (x, (-n))
+            return 1/result
+        return result

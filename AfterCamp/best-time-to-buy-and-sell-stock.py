3c788bbfabf3439c58a0aa3f98eabfcbbe72ad10
AbigailF1 <115185297+AbigailF1@users.noreply.github.com>
2023-11-02 12:10:59 +0300

    Added solution for Best Time to Buy and Sell Stock

---
+++ AfterCamp/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        sell  = 0
+        for i in range(1,len(prices)):
+           if buy > prices[i]:
+               buy = prices[i]
+           else:
+               sell = max(sell , prices[i] - buy)
+        return sell
+
+        
